703fc3878da3218254d1bfad06eb2a96b82a7db5
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-11-15 17:42:04 -0300

    Add files in project

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:playflow/shared/themes/app_colors.dart';
+import 'package:playflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  SplashPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull)),
+        ],
+      ),
+    );
+  }
+}
